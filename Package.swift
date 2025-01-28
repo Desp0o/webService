2609f6657f3bd4f78f699eb8c1dc73b2bead626e
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2025-01-28 19:44:09 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkManagerFramework",
-            platforms: [.iOS(.v15)],
             targets: ["NetworkManagerFramework"]),
     ],
     targets: [
